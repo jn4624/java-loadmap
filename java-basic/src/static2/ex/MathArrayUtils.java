6abd9d9ba3994b15fa123ee7e12250d04ff8c7fd
LimJiNa <jn4624@gmail.com>
2024-07-10 00:40:00 +0900

    실전 자바 - 기본편 - 자바 메모리 구조와 static

---
+++ java-basic/src/static2/ex/MathArrayUtils.java
@@ -0,0 +1,35 @@
+package static2.ex;
+
+public class MathArrayUtils {
+    public static int sum(int[] values) {
+        int sum = 0;
+        for (int value : values) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static double average(int[] values) {
+        return (double) sum(values) / values.length;
+    }
+
+    public static int min(int[] values) {
+        int min = Integer.MAX_VALUE;
+        for (int value : values) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] values) {
+        int max = Integer.MIN_VALUE;
+        for (int value : values) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+}
