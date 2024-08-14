dab61d7e4f660cb83d31a7ac759e43bfa21fce62
jn4624 <jn4624@gmail.com>
2024-08-15 00:36:54 +0900

    실전 자바 - 중급 2편 - 컬렉션 프레임워크(LinkedList)

---
+++ java-intermediate-second/src/collection/link/MyLinkedListV2Main.java
@@ -0,0 +1,36 @@
+package collection.link;
+
+public class MyLinkedListV2Main {
+    public static void main(String[] args) {
+        MyLinkedListV2 list = new MyLinkedListV2();
+        // 마지막에 추가 O(n)
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+        System.out.println();
+
+        // 첫번째 항목에 추가 O(1)
+        System.out.println("첫번째 항목에 추가");
+        list.add(0, "d");
+        System.out.println(list);
+        System.out.println();
+
+        // 첫번째 항목 삭제 O(1)
+        System.out.println("첫번째 항목 삭제");
+        list.remove(0);
+        System.out.println(list);
+        System.out.println();
+
+        // 중간 항목에 추가 O(n)
+        System.out.println("중간 항목에 추가");
+        list.add(1, "e");
+        System.out.println(list);
+        System.out.println();
+
+        // 중간 항목 삭제 O(n)
+        System.out.println("중간 항목 삭제");
+        list.remove(1);
+        System.out.println(list);
+    }
+}
