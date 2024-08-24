e49a189c33b103071c3dcef194c696b771693160
jn4624 <jn4624@gmail.com>
2024-08-25 00:48:52 +0900

    실전 자바 - 고급 1편 - 스레드 제어와 생명 주기 2

---
+++ java-advanced-first/src/thread/control/interrupt/ThreadStopMainV4.java
@@ -0,0 +1,38 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV4 {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(100);
+        log("작업 중단 지시 thread.interrupt");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태 1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            // 인터럽트 상태 확인 후 인터럽트 상태까지 변경한다.
+            while (!Thread.interrupted()) {
+                log("작업 중");
+            }
+            log("work 스레드 인터럽트 상태 2 = " + Thread.currentThread().isInterrupted());
+
+            try {
+                log("자원 정리 시도");
+                Thread.sleep(1000);
+                log("자원 정리 완료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 = 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태 3 = " + Thread.currentThread().isInterrupted());
+            }
+            log("작업 종료");
+        }
+    }
+}
